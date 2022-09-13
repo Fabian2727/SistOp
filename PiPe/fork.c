327d31e6304e0ff92843f738568c76a2bafb6234
Fabian2727 <80995242+Fabian2727@users.noreply.github.com>
2022-09-13 16:36:53 -0500

    Add files via upload

---
+++ PiPe/fork.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+int pid;
+pid = fork();
+
+//Child proccess
+if (pid == 0)
+{
+system("echo 'hijo'");
+system("echo $$");
+sleep(100);
+}else{
+system ("echo 'padre'");
+system("echo $$");
+sleep(100);
+}
+return 0;
+}
