476aafe33226ef1a10027acd9068a8832891d25e
Fabian2727 <80995242+Fabian2727@users.noreply.github.com>
2022-09-13 16:45:09 -0500

    Update fork.c

--- PiPe/fork.c
+++ PiPe/fork.c
@@ -8,13 +8,19 @@ pid = fork();
 //Child proccess
 if (pid == 0)
 {
-system("echo 'hijo'");
-system("echo $$");
-sleep(100);
-}else{
-system ("echo 'padre'");
-system("echo $$");
-sleep(100);
+   system("echo 'hijo'");
+   int pid = getpid();
+   int ppid = getppid();
+   system("echo 'hijo'");
+   printf("Pdre ID: %d, PPID: %d /n",pid,ppid);
+   sleep(100);
+}
+else{
+   system("echo 'hijo'");
+   int pid = getpid();
+   int ppid = getppid();
+   printf("Pdre ID: %d, PPID: %d /n",pid,ppid);
+   sleep(100);
 }
 return 0;
 }
